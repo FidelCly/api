053c9f4617ae166407c7a21607900bc804a4d2eb
sihamais <sihamais98@gmail.com>
2022-12-08 15:53:00 +0100

    fix(promotion-counter): entity

--- src/entities/promotion-counter.ts
+++ src/entities/promotion-counter.ts
@@ -1,35 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { IsBoolean, IsNotEmpty, IsNumber } from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional } from "class-validator";
 import {
   Column,
   CreateDateColumn,
   Entity,
   JoinColumn,
-  OneToMany,
-  OneToOne,
   ManyToOne,
-  PrimaryColumn,
   UpdateDateColumn,
   PrimaryGeneratedColumn,
 } from "typeorm";
 import { Promotion } from "./promotion";
-import { Shop } from "./shop";
 import { User } from "./user";
 
-@Entity({ name: "promotions-counter" }) // table name in database
+@Entity({ name: "promotion-counters" }) // table name in database
 export class PromotionCounter {
   @PrimaryGeneratedColumn()
-  @IsNumber()
-  Id: number;
+  id: number;
 
-  @IsNotEmpty()
   @IsNumber()
-  @Column({ nullable: false })
-  shopId: number;
-
-  @OneToMany(() => Shop, (shop: Shop) => shop.id)
-  @JoinColumn()
-  shop!: Shop;
+  @IsOptional()
+  @Column({ default: 0 })
+  counter: number;
 
   @IsNotEmpty()
   @IsNumber()
@@ -41,25 +32,13 @@ export class PromotionCounter {
   user!: User;
 
   @IsNotEmpty()
-  @PrimaryColumn()
+  @IsNumber()
   @Column({ nullable: false })
   promotionId: number;
 
-  @OneToOne(() => Promotion, (promotion: Promotion) => promotion.id)
+  @ManyToOne(() => Promotion, (promotion: Promotion) => promotion.id)
   @JoinColumn()
   promotion!: Promotion;
-
-  @IsNumber()
-  @Column({ default: 0 })
-  increment: number;
-
-  @IsBoolean()
-  @Column({ default: true })
-  isActive: boolean;
-
-  @IsNumber()
-  @Column({ default: 0 })
-  nbValidation: number;
 
   @CreateDateColumn()
   createAt!: Date;
