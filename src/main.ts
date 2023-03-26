db40b767689d43622006ac7c6136e06ca06d7f90
sihamais <sihamais98@gmail.com>
2023-03-26 21:30:58 +0200

    fix: configure server port

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['verbose'],
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(3000);
+  await app.listen(+process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
