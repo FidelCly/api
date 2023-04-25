276aaacf5dcaf3e7f4067d7b464fc971601a823f
Andi MANIETA <andimanieta@gmail.com>
2023-04-25 21:06:56 +0200

    Fix migration changes

--- build/tests/seeds/user.seed.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userFixture = void 0;
-exports.userFixture = {
-    username: "testUsername",
-    email: "testEmail@email.com",
-};
-//# sourceMappingURL=user.seed.js.map
