9b52ad5316ccb700be0efa2fb9b10e91ce12de44
Andi MANIETA <andimanieta@gmail.com>
2023-07-13 14:35:47 +0200

    fix: proto files

--- src/balance/balance.service.ts
+++ src/balance/balance.service.ts
@@ -11,8 +11,9 @@ import { Balance } from './balance.entity';
 import {
   BALANCE_SERVICE_NAME,
   BalanceServiceClient,
-} from '../analytics/balance.pb';
-import { ClientGrpc } from '@nestjs/microservices';
+} from '../analytics/analytics.pb';
+import { CreateBalanceDto, UpdateBalanceDto } from './balance.dto';
+import { Balance } from './balance.entity';
 
 @Injectable()
 export class BalanceService {
